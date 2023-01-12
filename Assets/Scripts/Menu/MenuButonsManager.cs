b38cddbe5fb1dd654a35519ed4f8433bb4371f17
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-01-11 21:36:55 -0300

    feat: add animation to menu buttons so they open one after the other

--- Assets/Scripts/Menu/MenuButonsManager.cs
+++ Assets/Scripts/Menu/MenuButonsManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuButonsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> buttons;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+    private void Awake()
     {
-        
+        HideAllButtons();
+        ShowButons();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideAllButtons()
     {
-        
+        foreach (var b in buttons)
+        {
+            b.transform.localScale = Vector3.zero;
+            b.SetActive(false);
+        }
     }
+
+    private void ShowButons()
+    {
+        //foreach (var b in buttons)
+        for(int i = 0; i < buttons.Count; i++)
+        {
+            var b = buttons[i];
+            b.SetActive(true);
+            b.transform.DOScale(1, duration).SetDelay(i * delay).SetEase(ease);
+        }
+    }
+
 }
