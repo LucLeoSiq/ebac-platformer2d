7b18f9107ae53bd24eb19f4e98c30ad721d591cd
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-01-12 20:34:48 -0300

    feat: implement player character walk prototype

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody2D;
+    public Vector2 velocity;
+    public float speed;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //myRigidbody2D.MovePosition(myRigidbody2D.position - velocity * Time.deltaTime);
+            myRigidbody2D.velocity = new Vector2(-speed, myRigidbody2D.velocity.y);
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //myRigidbody2D.MovePosition(myRigidbody2D.position + velocity * Time.deltaTime);
+            myRigidbody2D.velocity = new Vector2(speed, myRigidbody2D.velocity.y);
+        }
+    }
+}
