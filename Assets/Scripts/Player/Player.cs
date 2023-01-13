4fc68b2120cbca25131ec415fc3d2f728c034e23
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-01-13 08:42:57 -0300

    feat: add run key to player character

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,7 +8,9 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidbody2D;
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float speedRun;
     public float forceJump = 10.0f;
+    private float _currentspeed;
 
     void Update()
     {
@@ -17,16 +20,26 @@ public class Player : MonoBehaviour
 
     private void HandleMovement()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+        { 
+            _currentspeed = speedRun; 
+        }
+        else
+        {
+            _currentspeed = speed;
+        }
+
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidbody2D.MovePosition(myRigidbody2D.position - velocity * Time.deltaTime);
-            myRigidbody2D.velocity = new Vector2(-speed, myRigidbody2D.velocity.y);
+            myRigidbody2D.velocity = new Vector2(-_currentspeed, myRigidbody2D.velocity.y);
         }
 
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidbody2D.MovePosition(myRigidbody2D.position + velocity * Time.deltaTime);
-            myRigidbody2D.velocity = new Vector2(speed, myRigidbody2D.velocity.y);
+            myRigidbody2D.velocity = new Vector2(_currentspeed, myRigidbody2D.velocity.y);
         }
 
         if(myRigidbody2D.velocity.x > 0)
