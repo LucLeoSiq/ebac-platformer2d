036001c6c3d6a6fb7085b83b3a3499802c0e3e6f
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-01-12 21:21:05 -0300

    feat: add prototype jump to player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody2D;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
     public float speed;
+    public float forceJump = 10.0f;
 
     void Update()
     {
         HandleMovement();
+        HandleJump();
     }
 
     private void HandleMovement()
@@ -25,6 +27,24 @@ public class Player : MonoBehaviour
         {
             //myRigidbody2D.MovePosition(myRigidbody2D.position + velocity * Time.deltaTime);
             myRigidbody2D.velocity = new Vector2(speed, myRigidbody2D.velocity.y);
+        }
+
+        if(myRigidbody2D.velocity.x > 0)
+        {
+            myRigidbody2D.velocity += friction;
+        }
+
+        else if(myRigidbody2D.velocity.x < 0)
+        {
+            myRigidbody2D.velocity -= friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * forceJump;
         }
     }
 }
