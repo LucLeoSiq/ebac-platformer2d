24675a278619df66d7f3a8a270e86b091b3b264c
Lucas Leonel Siqueira <lucasleosiq@gmail.com>
2023-01-27 08:14:37 -0300

    feat: add multiple sound sources to player walk

--- Assets/Scripts/Audio/AudioRandomPlayAudioClips.cs
+++ Assets/Scripts/Audio/AudioRandomPlayAudioClips.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class AudioRandomPlayAudioClips : MonoBehaviour
 {
     public List<AudioClip> audioClipList;
-    public AudioSource audioSource;
+    public List<AudioSource> audioSourceList;
+    private int _index = 0; 
 
     public void PlayRandom()
     {
+        if (_index >= audioSourceList.Count) _index = 0;
+
+        var audioSource = audioSourceList[_index];
+
         audioSource.clip = audioClipList[Random.Range(0, audioClipList.Count)];
-        audioSource.Play(); 
+        audioSource.Play();
+
+        _index++;
     }
 }
